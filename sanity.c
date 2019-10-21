e17567ce0c75fd830c4c91b3b8512bceee1fc84a
Rafael Pardini <rafael.pardini@yahoo.com.br>
2019-10-20 21:00:13 -0300

    finishing sanity.c

--- sanity.c
+++ sanity.c
@@ -5,37 +5,25 @@
 #define S_BOUND 1
 #define IO_BOUND 2
 
-typedef struct procStatistics procStatistics;
-
-struct procStatistics {
-  int pid;
-  int type;
-  int retime;
-  int rutime;
-  int stime;
-};
-
 void sanity(int n) {
   int counter0;
   int counter1;
   int counter2;
 
-  int index = -1;
-
   int pid;
   int type;
   int retime;
   int rutime;
   int stime;
 
+  int statistics[3][3];
+  for(counter0 = 0; counter0 < 3; counter0++)
+    for(counter1 = 0; counter1 < 3; counter1++)
+      statistics[counter0][counter1] = 0;
+
   for(counter0 = 0; counter0 < 3 * n; counter0++) {
-    index++;
     pid = fork();
-    type = pid % 3;
-
     if(pid == 0) {
-      printf(1, "index:%d, pid:%d\n", index, pid);
-
       type = (pid - 4) % 3;
 
       switch(type) {
@@ -43,7 +31,6 @@ void sanity(int n) {
           for(counter1 = 0; counter1 < 100; counter1++) {
             for(counter2 = 0; counter2 < 1000000; counter2++) {}
           }
-          set_prio(1);
           break;
 
         case S_BOUND:
@@ -51,14 +38,12 @@ void sanity(int n) {
             for(counter2 = 0; counter2 < 1000000; counter2++) {}
             //yield();
           }
-          set_prio(2);
           break;
 
         case IO_BOUND:
           for(counter1 = 0; counter1 < 100; counter1++) {
             sleep(1);
           }
-          set_prio(3);
           break;
       }
       exit();
@@ -67,41 +52,41 @@ void sanity(int n) {
   }
 
   for(counter0 = 0; counter0 < 3 * n; counter0++) {
-      pid = wait2(&retime, &rutime, &stime);
-      int modulus = (pid - 4) % 3; // corresponds to types in the previous loop
-      switch(res) {
-        case CPU_BOUND:
-          printf(1, "CPU-bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
-          //sums[0][0] += retime;
-          //sums[0][1] += rutime;
-          //sums[0][2] += stime;
-          break;
-        case S_BOUND:
-          printf(1, "CPU-S bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
-          //sums[1][0] += retime;
-          //sums[1][1] += rutime;
-          //sums[1][2] += stime;
-          break;
-        case IO_BOUND:
-          printf(1, "I/O bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
-          //sums[2][0] += retime;
-          //sums[2][1] += rutime;
-          //sums[2][2] += stime;
-          break;
-      }
+    pid = wait2(&retime, &rutime, &stime);
+    type = (pid - 4) % 3; // corresponds to types in the previous loop
+    switch(type) {
+      case CPU_BOUND:
+        printf(1, "pid: %d, tipo: CPU_BOUND, tempo de espera: %d, tempo executando: %d, tempo de io: %d\n", pid, retime, rutime, stime);
+        statistics[CPU_BOUND][0] += stime;
+        statistics[CPU_BOUND][1] += retime;
+        statistics[CPU_BOUND][2] += stime + retime + rutime;
+        break;
+
+      case S_BOUND:
+        printf(1, "pid: %d, tipo: S_BOUND, tempo de espera: %d, tempo executando: %d, tempo de io: %d\n", pid, retime, rutime, stime);
+        statistics[CPU_BOUND][0] += stime;
+        statistics[CPU_BOUND][1] += retime;
+        statistics[CPU_BOUND][2] += stime + retime + rutime;
+        break;
+
+      case IO_BOUND:
+        printf(1, "pid: %d, tipo: IO_BOUND, tempo de espera: %d, tempo executando: %d, tempo de io: %d\n", pid, retime, rutime, stime);
+        statistics[CPU_BOUND][0] += stime;
+        statistics[CPU_BOUND][1] += retime;
+        statistics[CPU_BOUND][2] += stime + retime + rutime;
+        break;
+    }
   }
 
-  /*
-    for (i = 0; i < 3; i++)
-          for (j = 0; j < 3; j++)
-                  sums[i][j] /= n;
-      printf(1, "\n\nCPU bound:\nAverage ready time: %d\nAverage running time: %d\nAverage sleeping time: %d\nAverage turnaround time: %d\n\n\n", sums[0][0], sums[0][1], sums[0][2], sums[0][0] + sums[0][1] + sums[0][2]);
-        printf(1, "CPU-S bound:\nAverage ready time: %d\nAverage running time: %d\nAverage sleeping time: %d\nAverage turnaround time: %d\n\n\n", sums[1][0], sums[1][1], sums[1][2], sums[1][0] + sums[1][1] + sums[1][2]);
-          printf(1, "I/O bound:\nAverage ready time: %d\nAverage running time: %d\nAverage sleeping time: %d\nAverage turnaround time: %d\n\n\n", sums[2][0], sums[2][1], sums[2][2], sums[2][0] + sums[2][1] + sums[2][2]);
-            exit();
-                  
-} */
-
+  printf(1, "CPU_BOUND: Tempo medio SLEEPING: %d\n", (float)statistics[CPU_BOUND][0] / 3.0);
+  printf(1, "CPU_BOUND: Tempo medio READY: %d\n", (float)statistics[CPU_BOUND][1] / 3.0);
+  printf(1, "CPU_BOUND: Tempo medio turnaround: %d\n", (float)statistics[CPU_BOUND][2] / 3.0);
+  printf(1, "S_BOUND: Tempo medio SLEEPING: %d\n", (float)statistics[S_BOUND][0] / 3.0);
+  printf(1, "S_BOUND: Tempo medio READY: %d\n", (float)statistics[S_BOUND][1] / 3.0);
+  printf(1, "S_BOUND: Tempo medio turnaround: %d\n", (float)statistics[S_BOUND][2] / 3.0);
+  printf(1, "IO_BOUND: Tempo medio SLEEPING: %d\n", (float)statistics[IO_BOUND][0] / 3.0);
+  printf(1, "IO_BOUND: Tempo medio READY: %d\n", (float)statistics[IO_BOUND][1] / 3.0);
+  printf(1, "IO_BOUND: Tempo medio turnaround: %d\n", (float)statistics[IO_BOUND][2] / 3.0);
 }
 
 int main(int argc, char* argv[]) {
